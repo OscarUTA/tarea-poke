575307457053f7e5ada738e39e38d6ec8522e0f6
OscarUTA <oscarflores_estudiante_@hotmail.com>
2022-07-11 06:06:52 -0500

    style:tabs, spaces to better display the content

--- tareas-poke/src/App.js
+++ tareas-poke/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Main from './componentes/Main';
+import './componentes/style.css'
 function App() {
   return (
     <>
-      
+      <Main/>
     </>
   );
 }
